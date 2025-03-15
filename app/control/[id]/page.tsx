3da9cb2c47e395c7ecad3ce1434890fa4c7b82f3
Mpho Motlhale <129661174+tshephomotlhale@users.noreply.github.com>
2025-03-15 11:45:22 +0200

    4th

--- app/control/[id]/page.tsx
+++ app/control/[id]/page.tsx
@@ -1,16 +1,7 @@
-import dynamic from "next/dynamic";
+"use client";
 
-// Dynamically import your client component so that it only renders on the client.
-const ControlPageClient = dynamic(() => import("@/components/control"), {
-  ssr: false,
-});
+import ControlPage from "@/components/control";
 
-export default function ControlPageWrapper({
-  params,
-}: {
-  params: { id: string };
-  searchParams?: Record<string, string | string[]>;
-}) {
-  // Now params is resolved on the server, and we pass the id to our client component.
-  return <ControlPageClient roomId={params.id} />;
+export default function ControlPageWrapper({ params }: { params: { id: string } }) {
+  return <ControlPage roomId={params.id} />;
 }
